ba62fab6e74573bc74256f21223d20daff3a9a2b
Shuguang Liu <shuguanl@microsoft.com>
2018-09-30 15:24:04 +0800

    stage

--- src/Python/Naropine/app/views.py
+++ src/Python/Naropine/app/views.py
@@ -3,5 +3,5 @@ dfrom app import app
 @app.route('/')
 @app.route('/index')
 def index() :
-	return "Hello, World! This is a great achievement!"
+	return "Hello, World! This is a great achievement! Cool"
 
