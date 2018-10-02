cfe31543725759041e353e5c529b4f9a5f3f915c
Shuguang Liu <shuguanl@microsoft.com>
2018-10-03 00:02:19 +0800

    index.html

--- src/Python/Naropine/app/views.py
+++ src/Python/Naropine/app/views.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
-
 @app.route('/')
 @app.route('/index')
 def index() :
-	return "Hello, World! This is a great achievement! Cool"
-
+	user = {'name' : 'Shuguang Liu' }
+	posts = [
+		{
+			'author': {'nickname' : 'Yun Ma'},
+			'body' : 'I am mom'
+		},
+		{
+			'author' : {'nickname' : "Yiru Liu"},
+			'body' : 'I am daughter'
+		}
+	]
+	return render_template("index.html",
+		title='Board',
+		user = user,
+		posts = posts) 
+	
