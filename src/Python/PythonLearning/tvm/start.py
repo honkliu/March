9d521001cf234fb517da3370f6721197eae8ded8
Shuguang Liu <shuguanl@microsoft.com>
2019-07-17 15:01:03 +0800

    start

--- src/Python/PythonLearning/tvm/start.py
+++ src/Python/PythonLearning/tvm/start.py
@@ -1,14 +1,23 @@
-import matplotlib.pyplot as plt 
 import numpy as np
 
-import tvm as tvm
+import tvm
+from tvm import rpc
+from tvm.contrib import util
 
-from tvm import relay
-from tvm.relay import testing
+n = tvm.convert(1024)
+A = tvm.placeholder((n,), name='A')
+B = tvm.compute((n,), lambda i: A[i] + 1.0, name='B')
+s = tvm.create_schedule(B.op)
 
-from tvm.contrib import graph_runtime
+local_demo = True
 
-x = np.linspace(0, 20, 100)
+if local_demo:
+    target = 'llvm'
+else:
+    target = 'llvm -target=armv7l-linux-gnueabihf'
 
-plt.plot(x, np.sin(x))
-plt.show()
+func = tvm.build(s, [A, B], target=target, name='add_one')
+# save the lib at a local temp folder
+temp = util.tempdir()
+path = temp.relpath('lib.tar')
+func.export_library(path)
