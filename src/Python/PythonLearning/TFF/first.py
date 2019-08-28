7e50e18c84d289ed08f1075257fd32a1a99bf622
Shuguang Liu <shuguanl@microsoft.com>
2019-08-28 14:56:55 +0800

    self

--- src/Python/PythonLearning/TFF/first.py
+++ src/Python/PythonLearning/TFF/first.py
@@ -116,6 +116,118 @@ state = iterative_process.initialize()
 state, metrics = iterative_process.next(state, federated_train_data)
 print('round  1, metrics={}'.format(metrics))
 
-for round_num in range(2,11):
+for round_num in range(2,5):
     state, metrics = iterative_process.next(state, federated_train_data)
     print('round  {:2d}, metrics={}'.format(round_num, metrics))
+
+def create_mnist_variables():
+    return MnistVariables(
+        weights = tf.Variable(
+            lambda:tf.zeros(dtype=tf.float32, shape=(784,10)),
+            name = 'Weights',
+            trainable = True),
+        bias = tf.Variable(
+            lambda: tf.zeros(dtype=tf.float32, shape=(10)),
+            name = 'bias',
+            trainable = True),
+        num_examples = tf.Variable(
+            0.0, 
+            name = 'num_examples',
+            trainable = False),
+        loss_sum = tf.Variable(
+            0.0,
+            name = 'loss_sum',
+            trainale= False),
+        accuracy_sum = tf.Variable(
+            0.0,
+            name = 'accuracy_sum',
+            trainale= False))
+
+def mnist_forward_pass(variables, batch):
+    y = tf.nn.softmax(tf.matmul(batch['x'], variables.weights) + variables.bias)
+
+    predictions = tf.cast(tf.argmax(y, 1), tf.int32)
+
+    flat_labels = tf.reshape(batch['y'], [-1])
+    loss = -tf.reduce_mean(tf.reduce_sum(tf.one_hot(flat_labels, 10) * tf.log(y), reduction_indices=[1]))
+    accuracy =tf.reduce_mean(tf.cast(tf.equal(predictions, flat_lables), tf.float32))
+
+    num_examples = tf.cast(tf.size(batch['y']), tf.float32)
+    variables.num_examples.assign_add(num_examples)
+    variables.loss_sum.assign_add(loss * num_examples)
+    variables.accuracy_sum.assign_add(accuracy * num_examples)
+    
+    return loss, predictions
+
+def get_local_mnist_metrics(variables):
+  return collections.OrderedDict([
+      ('num_examples', variables.num_examples),
+      ('loss', variables.loss_sum / variables.num_examples),
+      ('accuracy', variables.accuracy_sum / variables.num_examples)
+    ])
+
+@tff.federated_computation
+def aggregate_mnist_metrics_across_clients(metrics):
+  return {
+      'num_examples': tff.federated_sum(metrics.num_examples),
+      'loss': tff.federated_mean(metrics.loss, metrics.num_examples),
+      'accuracy': tff.federated_mean(metrics.accuracy, metrics.num_examples)
+  }
+
+class MnistModel(tff.learning.Model):
+
+  def __init__(self):
+    self._variables = create_mnist_variables()
+
+  @property
+  def trainable_variables(self):
+    return [self._variables.weights, self._variables.bias]
+
+  @property
+  def non_trainable_variables(self):
+    return []
+
+  @property
+  def local_variables(self):
+    return [
+        self._variables.num_examples, self._variables.loss_sum,
+        self._variables.accuracy_sum
+    ]
+
+  @property
+  def input_spec(self):
+    return collections.OrderedDict([('x', tf.TensorSpec([None, 784],
+                                                        tf.float32)),
+                                    ('y', tf.TensorSpec([None, 1], tf.int32))])
+
+  @tf.function
+  def forward_pass(self, batch, training=True):
+    del training
+    loss, predictions = mnist_forward_pass(self._variables, batch)
+    return tff.learning.BatchOutput(loss=loss, predictions=predictions)
+
+  @tf.function
+  def report_local_outputs(self):
+    return get_local_mnist_metrics(self._variables)
+
+  @property
+  def federated_output_computation(self):
+    return aggregate_mnist_metrics_across_clients
+
+class MnistTrainableModel(MnistModel, tff.learning.TrainableModel):
+
+  @tf.function
+  def train_on_batch(self, batch):
+    output = self.forward_pass(batch)
+    optimizer = tf.train.GradientDescentOptimizer(0.02)
+    optimizer.minimize(output.loss, var_list=self.trainable_variables)
+    return output
+
+iterative_process = tff.learning.build_federated_averaging_process(
+    MnistTrainableModel)
+
+ state = iterative_process.initialize()
+
+ #@test {"timeout": 600, "output": "ignore"}
+state, metrics = iterative_process.next(state, federated_train_data)
+print('round  x1, metrics={}'.format(metrics))   
